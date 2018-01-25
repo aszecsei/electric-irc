deea516c3057e396b728377ca141468abc7fc38c
Alic Szecsei <aszecsei@gmail.com>
2018-01-24 22:26:28 -0600

    Made the code pretty

--- app/renderer/components/titlebar.tsx
+++ app/renderer/components/titlebar.tsx
@@ -2,16 +2,16 @@ import * as React from 'react'
 import * as classNames from 'classnames'
 
 export interface ITitlebarProps {
-  handleClose?(event: any): void;
-  handleMinimize?(event: any): void;
-  handleMaximize?(event: any): void;
-  draggable?: boolean;
+  handleClose?(event: any): void
+  handleMinimize?(event: any): void
+  handleMaximize?(event: any): void
+  draggable?: boolean
 }
 
 export class Titlebar extends React.Component<ITitlebarProps, any> {
   constructor(props: ITitlebarProps) {
-    super(props);
-    this.state = {draggable: this.props.draggable}
+    super(props)
+    this.state = { draggable: this.props.draggable }
   }
 
   // prevent event
@@ -21,22 +21,31 @@ export class Titlebar extends React.Component<ITitlebarProps, any> {
   }
 
   public render() {
-    let classes = classNames('titlebar',
-    {
+    let classes = classNames('titlebar', {
       'webkit-draggable': this.state.draggable
-    });
+    })
     return (
       <div className={classes} id="titlebar">
         <div className="titlebar-stoplight">
-          <div onDoubleClick={this.handleNop} onClick={this.props.handleClose} className="titlebar-close">
+          <div
+            onDoubleClick={this.handleNop}
+            onClick={this.props.handleClose}
+            className="titlebar-close"
+          >
             {/* TODO: Fix this styling <i className="material-icons">close</i> */}
           </div>
-          <div onDoubleClick={this.handleNop} onClick={this.props.handleMinimize} className="titlebar-minimize"></div>
-          <div onDoubleClick={this.handleNop} onClick={this.props.handleMaximize} className="titlebar-maximize"></div>
+          <div
+            onDoubleClick={this.handleNop}
+            onClick={this.props.handleMinimize}
+            className="titlebar-minimize"
+          />
+          <div
+            onDoubleClick={this.handleNop}
+            onClick={this.props.handleMaximize}
+            className="titlebar-maximize"
+          />
         </div>
-        <div className="titlebar-children">
-          {this.props.children}
-        </div>
+        <div className="titlebar-children">{this.props.children}</div>
       </div>
     )
   }
