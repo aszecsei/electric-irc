2b4291d83e15c2a01e2e6f86235e19cf86d6e21a
Alic Szecsei <aszecsei@gmail.com>
2018-02-01 11:21:00 -0600

    Move IRC client to Window component

--- app/renderer/index.tsx
+++ app/renderer/index.tsx
@@ -5,11 +5,43 @@ import { remote } from 'electron'
 
 import { Titlebar } from './components/titlebar'
 
+import * as irc from 'irc'
+
 import 'material-design-icons/iconfont/material-icons.css'
 import 'typeface-roboto/index.css'
 import './stylesheets/main.scss'
+import { IMessage } from 'irc'
 
-export class Window extends React.Component {
+interface IWindowState {
+  log: Array<string>
+}
+
+export class Window extends React.Component<any, IWindowState> {
+  client: irc.Client
+
+  constructor(props: any) {
+    super(props)
+    this.client = new irc.Client('chat.freenode.com', 'ElectricIRC', {
+      channels: ['#electric-irc']
+    })
+    // console.log(this.client)
+    this.state = {
+      log: []
+    }
+    this.client.addListener('raw', (message: irc.IMessage) => {
+      this.appendToLog(JSON.stringify(message))
+    })
+    this.client.addListener('error', (message: IMessage) => {
+      this.appendToLog('ERROR: ' + message)
+    })
+  }
+
+  appendToLog(message: string) {
+    this.setState((prevState: IWindowState) => {
+      return { log: [...prevState.log, message] }
+    })
+  }
+
   handleClose(e: any) {
     const window = remote.getCurrentWindow()
     window.close()
@@ -41,7 +73,7 @@ export class Window extends React.Component {
           Electric IRC
         </Titlebar>
         <div id="content">
-          <div>Hello, world! ❤❤❤</div>
+          {this.state.log.map((s, i) => <p key={i}>{s}</p>)}
         </div>
       </div>
     )
