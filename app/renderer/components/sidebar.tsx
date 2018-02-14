f18f5d94aaf616e32d23bb5c0f3cbba4c9b30b25
Tanner Brunscheon <tannerbrunscheon@gmail.com>
2018-02-13 22:35:52 -0600

    Made a sexy sidebar and made the icon work

--- app/renderer/components/sidebar.tsx
+++ app/renderer/components/sidebar.tsx
@@ -1,50 +1,89 @@
 import * as React from 'react'
-
 export class Sidebar extends React.Component<any, any> {
   public render() {
     return (
-      <div className="wrapper col-2 flex row">
-        <nav id="sidebar">
-          <div className="sidebar-header">
-            <h3>Collapsible Sidebar</h3>
-          </div>
+      <nav className="col-w flex" id="sidebar">
+        <div className="sidebar-header">
+          <h3>Electric IRC: Welcome Home</h3>
+        </div>
 
-          <ul className="list-unstyled components">
-            <li className="active">
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a
-                href="#homeSubmenu"
-                data-toggle="collapse"
-                aria-expanded="false"
-              >
-                Pages
-              </a>
-              <ul className="collapse list-unstyled" id="homeSubmenu">
-                <li>
-                  <a href="#">Page</a>
-                </li>
-                <li>
-                  <a href="#">Page</a>
-                </li>
-                <li>
-                  <a href="#">Page</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#">Portfolio</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+        <ul className="list-unstyled components">
+          <li className="active">
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">
+              IRC 1
+              <i className="material-icons ml-auto">expand_more</i>
+            </a>
+            <ul className="collapse list-unstyled" id="homeSubmenu">
+              <li>
+                <a href="#">Page</a>
+              </li>
+              <li>
+                <a href="#">Page</a>
+              </li>
+              <li>
+                <a href="#">Page</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">
+              IRC 2
+              <i className="material-icons ml-auto">expand_more</i>
+            </a>
+            <ul className="collapse list-unstyled" id="homeSubmenu">
+              <li>
+                <a href="#">Page</a>
+              </li>
+              <li>
+                <a href="#">Page</a>
+              </li>
+              <li>
+                <a href="#">Page</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">
+              IRC 3
+              <i className="material-icons ml-auto">expand_more</i>
+            </a>
+            <ul className="collapse list-unstyled" id="homeSubmenu">
+              <li>
+                <a href="#">Page</a>
+              </li>
+              <li>
+                <a href="#">Page</a>
+              </li>
+              <li>
+                <a href="#">Page</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">
+              IRC 4
+              <i className="material-icons ml-auto">expand_more</i>
+            </a>
+            <ul className="collapse list-unstyled" id="homeSubmenu">
+              <li>
+                <a href="#">Page</a>
+              </li>
+              <li>
+                <a href="#">Page</a>
+              </li>
+              <li>
+                <a href="#">Page</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+        </ul>
+      </nav>
     )
   }
 }
