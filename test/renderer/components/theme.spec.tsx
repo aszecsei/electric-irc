8447ebdba4060f0105c84f8c13b866b86a12a46f
Tanner Brunscheon <tannerbrunscheon@gmail.com>
2018-04-09 20:58:17 -0500

    Try again

--- test/renderer/components/theme.spec.tsx
+++ test/renderer/components/theme.spec.tsx
@@ -5,11 +5,21 @@ import * as React from 'react'
 import { mount, render, shallow, ReactWrapper } from 'enzyme'
 
 import { Theme } from '../../../app/renderer/components/theme'
+import { SettingFactory } from '../../../app/renderer/models/settings'
+import * as sinon from 'sinon'
+import { Map } from 'immutable'
+import * as themes from '../../../app/renderer/stylesheets/thememaps/themes'
 
 use(chaiEnzyme())
-
 describe('theme component', function() {
+  before(() => {
+    properties = themes.backup
+    wrapper = mount(<Theme properties={properties} />)
+  })
   it('should exist', function() {
     expect(Theme).to.exist
+  })
+  it('should exist', function() {
+    wrapper.expect(wrapper.ComponentDidMount()).to.be.called
   })
 })
