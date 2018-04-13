1c8c135f2ea42759fab016d4023b31c3005c44de
willem-dejong <willem-dejong@uiowa.edu>
2018-04-12 22:25:38 -0500

    open window prep

--- app/renderer/components/message.tsx
+++ app/renderer/components/message.tsx
@@ -6,9 +6,42 @@ import { Message } from '../models'
 interface IMessageProps {
   message: Message
 }
+function test(event: any) {
+  event.preventDefault()
+  var shell = require('shell')
+  shell.openExternal(event.target.href)
+}
+const urlre = /((https?:\/\/)|([a-z0-9]([%0-9a-z\-_~]*[a-z0-9])?@))?[a-z0-9]([%0-9a-z\-_~]*[a-z0-9])?(\.[a-z0-9]([%0-9a-z\-_~]*[a-z0-9])?)+(\:[0-9]+)?(\/[%0-9a-z\-_~.]*)*(\?[^\s]*)?/i
+//puts a <a> tag aroung links. keep <text> i purposly made my own html tag, it does not break anything and no errors are caused
+// (it's just a warning) the way the framwork works doesn't let me do stuff in loops/maps that return a mix of strings and html
+//tags so in order to keep the messages from being formated and styled oddly by the browser implimintation by tags I use a implemented tag
+//alls this means is the browser does no self formating on it only css will effect it. this means the text looks as though it was in one
+//element
+function link_process(str: string) {
+  var reres = urlre.exec(str)
+  var tempstr = str
+  var x = []
+  while (reres) {
+    if (reres.index > 0) {
+      x.push(<text>{emoji_process(tempstr.substring(0, reres.index))}</text>)
+    }
+    x.push(
+      <a href={reres[0]} onClick={test}>
+        {reres[0]}
+      </a>
+    )
+    const i = reres.index + reres[0].length
+    tempstr = tempstr.substring(i)
+    reres = urlre.exec(tempstr)
+  }
+  if (tempstr.length != 0) {
+    x.push(<text>{emoji_process(tempstr)}</text>)
+  }
+  return x
+}
 function image_process(str: string) {
-  const imgulrre = /https?:\/\/.*\.(jpg|jpeg|png|gif|bmp|img)$/i
-  var reres = imgulrre.exec(str)
+  const imgurlre = /https?:\/\/[^\s]+\.(jpg|jpeg|png|gif|bmp|img)/i
+  var reres = imgurlre.exec(str)
   if (reres) {
     return <img className="imgPrev" src={reres[0]} />
   }
@@ -31,9 +64,11 @@ function emoji_process(str: string): string {
 function has_sender(message: Message) {
   return (
     <p className="mmessage">
-      {message.sender + ': '}
+      {link_process(message.sender)}
+      <text>{': '}</text>
       <br />
-      <b className="mmessagetext">{emoji_process(message.text)}</b>
+      <b className="mmessagetext">{link_process(message.text)}</b>
+      <br />
       {image_process(message.text)}
     </p>
   )
@@ -42,7 +77,8 @@ function has_sender(message: Message) {
 function no_sender(message: Message) {
   return (
     <p className="mmessage">
-      <b className="mmessagetext">{emoji_process(message.text)}</b>
+      <b className="mmessagetext">{link_process(message.text)}</b>
+      <br />
       {image_process(message.text)}
     </p>
   )
