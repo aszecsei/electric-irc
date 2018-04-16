70d3c35df1524afa6220b81d5d3d7770cb8d82c2
Tanner Brunscheon <tannerbrunscheon@gmail.com>
2018-04-16 17:44:16 -0500

    Fixed setting factory

--- test/renderer/components/theme.spec.tsx
+++ test/renderer/components/theme.spec.tsx
@@ -29,7 +29,7 @@ describe('theme component', function() {
     expect(Theme).to.exist
   })
   it('should change state of name if valid', function() {
-    instance.componentWillReceiveProps(themes.backup)
+    instance.changeTheme('light')
     expect(instance.handleNewProperties()).to.be.calledOnce
   })
 })
