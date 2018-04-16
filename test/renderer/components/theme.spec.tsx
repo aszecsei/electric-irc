dc222689085f740ac9e3900699e3a942a1bf14f2
Tanner Brunscheon <tannerbrunscheon@gmail.com>
2018-04-16 17:19:23 -0500

    Local test hangs

--- test/renderer/components/theme.spec.tsx
+++ test/renderer/components/theme.spec.tsx
@@ -9,26 +9,27 @@ import { SettingFactory } from '../../../app/renderer/models/settings'
 import * as sinon from 'sinon'
 import { Map } from 'immutable'
 import * as themes from '../../../app/renderer/stylesheets/thememaps/themes'
+import * as AddModal from '../../../app/renderer/components/addmodal'
 
 use(chaiEnzyme())
 describe('theme component', function() {
   let properties = themes.backup
   let wrapper: ReactWrapper = null
+  let instance: any = null
+  let theme: any = null
   before(() => {
     wrapper = mount(
       <Theme properties={properties}>
         <div />
       </Theme>
     )
+    instance = wrapper.instance()
   })
   it('should exist', function() {
     expect(Theme).to.exist
   })
-  it('should exist', function() {
-    const theme = sinon.stub(Theme.prototype, 'handleNewProperties')
-    theme.reset()
-    let themein = themes.backup
-    wrapper.setProps({ themein })
-    expect(theme).to.have.been.called()
+  it('should change state of name if valid', function() {
+    instance.componentWillReceiveProps(themes.theme.get('light'))
+    expect(instance.handleNewProperties()).to.be.calledOnce
   })
 })
