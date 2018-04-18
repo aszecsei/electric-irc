1fef59fa9c5172ee88e60133a28590e9420de6df
willem-dejong <willem-dejong@uiowa.edu>
2018-04-18 16:01:11 -0500

    xx

--- app/renderer/components/addchannelmodal.tsx
+++ app/renderer/components/addchannelmodal.tsx
@@ -38,74 +38,74 @@ export class AddChannelModal extends React.Component<
     this.state = { ...defaultState }
   }
 
-  // handleAddChannel = (event: any) => {
-  //   this.setState({
-  //     channels: this.state.channels.push('')
-  //   })
-  // }
+  // // handleAddChannel = (event: any) => {
+  // //   this.setState({
+  // //     channels: this.state.channels.push('')
+  // //   })
+  // // }
 
-  generateHandleChangeChannel = () => {
-    return (event: any) => {
-      this.setState({
-        channel: event.target.value
-      })
-    }
-  }
-  // generateHandleDeleteChannel = (index: number) => {
+  // generateHandleChangeChannel = () => {
   //   return (event: any) => {
   //     this.setState({
-  //       channels: this.state.channels.remove(index)
+  //       channel: event.target.value
   //     })
   //   }
   // }
+  // // generateHandleDeleteChannel = (index: number) => {
+  // //   return (event: any) => {
+  // //     this.setState({
+  // //       channels: this.state.channels.remove(index)
+  // //     })
+  // //   }
+  // // }
 
-  handleSubmit = (event: any) => {
-    this.props.onAddChannelSubmit()
-    this.setState({ ...defaultState })
-    event.preventDefault()
-    this.props.onAddChannelToggle()
-  }
-  public render() {
-    return (
-      <Modal
-        isOpen={this.props.connid !== undefined}
-        toggle={this.props.onAddChannelToggle}
-        id="addchannelmodal"
-      >
-        <Form onSubmit={this.handleSubmit}>
-          <ModalHeader toggle={this.props.onAddChannelToggle}>
-            Add new server
-          </ModalHeader>
-          <ModalBody>
-            <FormGroup>
-              <InputGroup>
-                <Input
-                  className={'Channel'}
-                  type="text"
-                  value={this.state.channel}
-                  onChange={this.generateHandleChangeChannel()}
-                  name={`channel`}
-                  id={`channel`}
-                  placeholder={'#'}
-                />
-              </InputGroup>
-            </FormGroup>
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" type="submit">
-              Add
-            </Button>{' '}
-            <Button
-              color="secondary"
-              type="button"
-              onClick={this.props.onAddChannelToggle}
-            >
-              Cancel
-            </Button>
-          </ModalFooter>
-        </Form>
-      </Modal>
-    )
-  }
+  // handleSubmit = (event: any) => {
+  //   this.props.onAddChannelSubmit()
+  //   this.setState({ ...defaultState })
+  //   event.preventDefault()
+  //   this.props.onAddChannelToggle()
+  // }
+  // public render() {
+  //   return (
+  //     <Modal
+  //       isOpen={this.props.connid !== undefined}
+  //       toggle={this.props.onAddChannelToggle}
+  //       id="addchannelmodal"
+  //     >
+  //       <Form onSubmit={this.handleSubmit}>
+  //         <ModalHeader toggle={this.props.onAddChannelToggle}>
+  //           Add new server
+  //         </ModalHeader>
+  //         <ModalBody>
+  //           <FormGroup>
+  //             <InputGroup>
+  //               <Input
+  //                 className={'Channel'}
+  //                 type="text"
+  //                 value={this.state.channel}
+  //                 onChange={this.generateHandleChangeChannel()}
+  //                 name={`channel`}
+  //                 id={`channel`}
+  //                 placeholder={'#'}
+  //               />
+  //             </InputGroup>
+  //           </FormGroup>
+  //         </ModalBody>
+  //         <ModalFooter>
+  //           <Button color="primary" type="submit">
+  //             Add
+  //           </Button>{' '}
+  //           <Button
+  //             color="secondary"
+  //             type="button"
+  //             onClick={this.props.onAddChannelToggle}
+  //           >
+  //             Cancel
+  //           </Button>
+  //         </ModalFooter>
+  //       </Form>
+  //     </Modal>
+  //   )
+  // }
 }
 export default AddChannelModal
