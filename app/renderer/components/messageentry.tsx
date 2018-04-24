c2d0869469b78d4de5618919286cdb44dd6ab2c3
willem-dejong <willem-dejong@uiowa.edu>
2018-04-24 05:10:24 -0500

    EMOJIS!!!!!!!!!!!!!

--- app/renderer/components/messageentry.tsx
+++ app/renderer/components/messageentry.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react'
-
+import { emoticons, Emojis } from '../emojis'
+import { Picker, EmojiData, emojiIndex } from 'emoji-mart'
 import { Connection } from '../models/connections'
 import { Channel } from '../models/channel'
+import { emoji_process } from './message'
+declare module 'emojione' {
+  export function shortnameToUnicode(str: string): string
+}
+import * as emojione from 'emojione'
 
 interface IChatBoxProps {
   connection?: Connection
@@ -10,6 +16,7 @@ interface IChatBoxProps {
 }
 interface IMessageEntryState {
   value: string
+  emoji_vis: boolean
 }
 export class MessageEntry extends React.Component<
   IChatBoxProps,
@@ -17,11 +24,14 @@ export class MessageEntry extends React.Component<
 > {
   constructor(props: IChatBoxProps) {
     super(props)
-    this.state = { value: '' }
+    this.state = { value: '', emoji_vis: false }
   }
 
   handleChange = (event: any) => {
-    this.setState({ value: event.target.value })
+    this.setState({
+      value: emoji_process(event.target.value),
+      emoji_vis: this.state.emoji_vis
+    })
   }
 
   handleSubmit = (event: any) => {
@@ -34,23 +44,60 @@ export class MessageEntry extends React.Component<
     } else {
       console.error('Tried to send message without connection or channel')
     }
-    this.setState({ value: '' })
+    this.setState({ value: '', emoji_vis: false })
     event.preventDefault()
   }
-
+  toggle_emoji = (event: any) => {
+    event.preventDefault()
+    this.setState({ value: this.state.value, emoji_vis: !this.state.emoji_vis })
+    return false
+  }
+  pick_emoji = (emoji: EmojiData) => {
+    for (const i of Object.keys(emoticons)) {
+      console.log(emoticons[+i], Emojis[emoticons[+i]])
+    }
+    this.setState({
+      value: this.state.value + emoji.native,
+      emoji_vis: this.state.emoji_vis
+    })
+  }
+  show() {
+    if (this.state.emoji_vis) {
+      // custum can be used to add custom emojis, if you wnat to has picker use emoji set other then the native
+      // check out 'set' option
+      return (
+        <Picker
+          custom={[]}
+          onClick={this.pick_emoji}
+          emoji={'zap'}
+          title={''}
+          native={true}
+        />
+      )
+    }
+    return
+  }
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Send Message:
+      <div className={'widthhund'}>
+        {this.show()}
+        <form className={'widthhund'} onSubmit={this.handleSubmit}>
+          <a href={'#'} onClick={this.toggle_emoji}>
+            ⚡
+          </a>
+          <label>Send Message:</label>
           <input
+            id={'messagebox'}
             type="text"
             value={this.state.value}
             onChange={this.handleChange}
+            onClick={event => {
+              this.setState({ value: this.state.value, emoji_vis: false })
+            }}
           />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     )
   }
 }
