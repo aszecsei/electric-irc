74ff9b0f37ca232b96e687d874664da293cb95d0
Alic Szecsei <aszecsei@gmail.com>
2018-04-26 12:04:41 -0500

    Add guid to file read/write operations

--- app/renderer/utilities/file-storage.ts
+++ app/renderer/utilities/file-storage.ts
@@ -6,12 +6,14 @@ import {
   SAVE_FILE,
   SAVE_FILE_COMPLETE
 } from '../../common/file-storage'
+import { Guid } from '../models';
 
 export function saveFile(filename: string, data: any) {
   return new Promise((resolve, reject) => {
-    ipcRenderer.send(SAVE_FILE, filename, data)
+    const saveGuid = Guid.create().toString()
+    ipcRenderer.send(SAVE_FILE, filename, data, saveGuid)
     ipcRenderer.once(
-      `${SAVE_FILE_COMPLETE}:${filename}`,
+      `${SAVE_FILE_COMPLETE}:${filename}:${saveGuid}`,
       (event: IpcMessageEvent, response: any) => {
         if (response.err) {
           reject(response.err)
@@ -25,9 +27,10 @@ export function saveFile(filename: string, data: any) {
 
 export function readFile(filename: string) {
   return new Promise((resolve, reject) => {
-    ipcRenderer.send(READ_FILE, filename)
+    const readGuid = Guid.create().toString()
+    ipcRenderer.send(READ_FILE, filename, readGuid)
     ipcRenderer.once(
-      `${READ_FILE_COMPLETE}:${filename}`,
+      `${READ_FILE_COMPLETE}:${filename}:${readGuid}`,
       (event: IpcMessageEvent, response: any) => {
         if (response.err) {
           reject(response.err)
